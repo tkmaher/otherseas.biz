9d156586d2bcba98e05c370fa9343b55be3c317d
Tomasz Maher <tkmaher@umich.edu>
2024-07-04 10:28:06 -0400

    added music + art

--- script/squarize.js
+++ script/squarize.js
@@ -17,15 +17,21 @@ function drag(e) {
 function zoomimg(e) {
 	var imgzoom = document.getElementById("imgzoom");
 	imgzoom.style.opacity = 1;
-	document.getElementById("enlarged").src = e.target.style.getPropertyValue("--url")
-	imgzoom.style.pointerEvents = "auto"
+	document.getElementById("enlarged").src = e.target.style.getPropertyValue("--url");
+	imgzoom.style.pointerEvents = "auto";
+	document.getElementById("title").innerHTML = e.target.style.getPropertyValue("--title");
+	document.getElementById("link").innerHTML = e.target.style.getPropertyValue("--link");
+	document.getElementById("link").href = e.target.style.getPropertyValue("--link");
 }
 
-async function squarize(arr) {
+async function squarize(arr, num, info) {
 	var index, card, w, h, zoom;
 	const out = arr.split('\n').filter(x => x.length);
+	if (info) {
+		description = info.split('\n').filter(x => x.length);
+	}
 	var parent = document.getElementById('parent');
-	for (let i = 0; i < 5; i++) {
+	for (let i = 0; i < num; i++) {
 		index = Math.floor(Math.random() * out.length);
 		var img = new Image();
 		img.src = out[index];
@@ -44,14 +50,20 @@ async function squarize(arr) {
 		card.className = "card";
 		card.id = i;
 		card.style = "--url:url(" + out[index] + "); --h:" + h + "; --w:" + w + "; --zoom:" + zoom + ';';
-		card.style.top = (50 + ((-2 + i) * 5)) + '%';
-		card.style.left = (50 + ((-2 + i) * 5)) + '%';
+		card.style.top = (50 + (-(num/2 - 0.5) + i) * 5) + '%';
+		card.style.left = (50 + (-(num/2 - 0.5) + i) * 5) + '%';
+
 		parent.appendChild(card);
 
 		zoom = document.createElement("div");
 		zoom.className = "zoom";
 		zoom.innerHTML = '🔍';
-		zoom.style = "--url:" + out[index] + ";";
+		zoom.style.setProperty("--url", out[index]);
+		if (info) {
+			zoom.style.setProperty("--title", description[index * 2]);
+			zoom.style.setProperty("--link", description[(index * 2)+1]);
+			description.splice(index*2, 2);
+		}
 		card.appendChild(zoom);
 
 		out.splice(index, 1);
