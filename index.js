399481be664559f6999f7a11ebda5ae2f8745d45
Tomasz Maher <tkmaher@umich.edu>
2024-07-09 20:46:36 -0400

    added bgs

--- index.js
+++ index.js
@@ -1,19 +1,82 @@
 const isMobile = (/Mobi|Android|iPhone/i.test(navigator.userAgent));
 var sidebar = document.getElementById("sidebar");
 var openButton = document.getElementById("openButton");
+var items = document.getElementById("items");
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 function open(e) {
     if (sidebar.style.left == "-100%") {
-      openButton.innerHTML = "𖣯";
+      openButton.innerHTML = "[𖣯]";
       sidebar.style.left = "-10vh";
     } else {
-      openButton.innerHTML = "🗺️";
+      openButton.innerHTML = "[🗺️]";
       sidebar.style.left = "-100%";
     }
 }
 
-if(isMobile) {
-  sidebar.hidden = "true";
+openButton.addEventListener("click", open);
+
+
+$(document).ready(function() {
+
+  if(isMobile) {
+    items.style.marginLeft = "1vh";
+    sidebar.hidden = "true";
+  }
+
+});
+
+/* async function draw() {
+
+  var c1 = document.getElementById("canvas1");
+  var c2 = document.getElementById("canvas2");
+  var ctx1 = c1.getContext("2d");
+  var ctx2 = c2.getContext("2d");
+
+
+  if (isMobile){
+    c1.style.left = '0%';
+    c2.style.left = '0%';
+  }
+
+  ctx1.strokeStyle = 'gray';
+  ctx2.strokeStyle = 'white';
+  ctx2.lineWidth = '15';
+  ctx2.textAlign = 'center';
+  ctx2.textAlign = 'center';
+  ctx2.fillStyle = "gray";
+  var x1,y1,x2,y2;
+  var w = c1.width;
+  var h = c1.height;
+  //while (true) {
+    x1 = 100;
+    y1 = Math.random() * (h - 80) + 80;
+    ctx1.beginPath();
+    var arr=['o','t','h','e','r','s','e','a','s', '1','.','c','o','m'];
+    ctx2.font = ((Math.random() * 100) + 50) + "px 'Courier New', monospace";
+    ctx2.fillText(arr[0], x1, y1);
+    for (i = 1; i < arr.length; i++) {
+      await sleep(30);
+      x2 = x1 + (w/(arr.length + 1));
+      y2 =Math.random() * (h - 80) + 80;
+      ctx2.font = ((Math.random() * 100) + 50) + "px 'Courier New', monospace";
+      ctx1.moveTo(x2, y2);
+      ctx1.lineTo(x1, y1);
+      ctx2.fillText(arr[i], x2, y2);
+      x1 = x2;
+      y1 = y2;
+      ctx1.stroke();
+    }
+
+    //await sleep(500);
+
+    //ctx1.clearRect(0, 0, w, h);
+    //ctx2.clearRect(0, 0, w, h);
+  //}
 }
 
-openButton.addEventListener("click", open);
+draw();
+*/
--- script/squarize.js
+++ script/squarize.js
@@ -6,7 +6,6 @@ var dragging = false;
 var current;
 var top_z = 1;
 
-
 function drag(e) {
 	if (dragging && current == e.target.id) {
 		e.target.style.top = (e.clientY) + "px";
@@ -24,73 +23,79 @@ function zoomimg(e) {
 	document.getElementById("link").href = e.target.style.getPropertyValue("--link");
 }
 
+async function load(url, iter, num, info, index) {
+	let parent = document.getElementById('parent');
+	var img = new Image();
+	img.src = url;
+	await img.decode();
+	if (img.height > img.width) {
+		w = '300px';
+		h = 'auto';
+		zoom = '400px auto';
+	} else {
+		w = 'auto';
+		h = '300px';
+		zoom = 'auto 400px';
+	}
+
+	card = document.createElement("div");
+	card.className = "card";
+	card.id = iter;
+	card.style = "--url:url(" + url + "); --h:" + h + "; --w:" + w + "; --zoom:" + zoom + ';';
+	card.style.top = (50 + (-(num/2 - 0.5) + iter) * 5) + 'vh';
+	card.style.left = (50 + (-(num/2 - 0.5) + iter) * 5) + 'vw';
+	card.style.zIndex = -1 * (num - iter);
+
+	parent.appendChild(card);
+
+	zoom = document.createElement("div");
+	zoom.className = "zoom";
+	zoom.innerHTML = '🔍';
+	zoom.style.setProperty("--url", url);
+	if (info) {
+		zoom.style.setProperty("--title", description[index * 2]);
+		zoom.style.setProperty("--link", description[(index * 2)+1]);
+		description.splice(index*2, 2);
+	}
+	card.appendChild(zoom);
+
+	card.addEventListener("mousedown", (e) => {
+		if (dragging == false) {
+			dragging = true;
+			current = e.target.id;
+			e.target.style.zIndex = top_z;
+			top_z++;
+			drag(e);
+			imgzoom.style.zIndex = top_z;
+		}
+	})
+	card.addEventListener("mousemove", drag);
+	card.addEventListener("mouseup", (e) => {dragging = false;})
+
+	zoom.addEventListener("click", zoomimg);
+}
+
 async function squarize(arr, num, info) {
 	var index, card, w, h, zoom;
 	const out = arr.split('\n').filter(x => x.length);
 	if (info) {
 		description = info.split('\n').filter(x => x.length);
 	}
-	var parent = document.getElementById('parent');
 	for (let i = 0; i < num; i++) {
 		index = Math.floor(Math.random() * out.length);
-		var img = new Image();
-		img.src = out[index];
-		await img.decode();
-		if (img.height > img.width) {
-			w = '300px';
-			h = 'auto';
-			zoom = '400px auto';
-		} else {
-			w = 'auto';
-			h = '300px';
-			zoom = 'auto 400px';
-		}
-
-		card = document.createElement("div");
-		card.className = "card";
-		card.id = i;
-		card.style = "--url:url(" + out[index] + "); --h:" + h + "; --w:" + w + "; --zoom:" + zoom + ';';
-		card.style.top = (50 + (-(num/2 - 0.5) + i) * 5) + 'vh';
-		card.style.left = (50 + (-(num/2 - 0.5) + i) * 5) + 'vw';
-
-		parent.appendChild(card);
-
-		zoom = document.createElement("div");
-		zoom.className = "zoom";
-		zoom.innerHTML = '🔍';
-		zoom.style.setProperty("--url", out[index]);
-		if (info) {
-			zoom.style.setProperty("--title", description[index * 2]);
-			zoom.style.setProperty("--link", description[(index * 2)+1]);
-			description.splice(index*2, 2);
-		}
-		card.appendChild(zoom);
-
+		load(out[index], i, num, info, index);
 		out.splice(index, 1);
-		card.addEventListener("mousedown", (e) => {
-			if (dragging == false) {
-				dragging = true;
-				current = e.target.id;
-				e.target.style.zIndex = top_z;
-				top_z++;
-				drag(e);
-				imgzoom.style.zIndex = top_z;
-			}
-		})
-		card.addEventListener("mousemove", drag);
-		card.addEventListener("mouseup", (e) => {dragging = false;})
-
-		zoom.addEventListener("click", zoomimg);
 	}
+	await sleep(1000);
 
 	document.getElementById("closeimg").addEventListener("click", (e) =>
-	{
-		imgzoom.style.pointerEvents = "none";
-		imgzoom.style.opacity = 0;
-	}
+		{
+			imgzoom.style.pointerEvents = "none";
+			imgzoom.style.opacity = 0;
+		}
 	)
 
-	document.getElementById("fade").style.opacity = '0';
-	await sleep(1000);
-	document.getElementById("fade").remove();
+	//document.getElementById("fade").style.opacity = '0';
+	//await sleep(1000);
+	//document.getElementById("fade").remove();
 }
