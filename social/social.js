d3535fbbcff52ed206e2fc5691b7d6d810033607
Tomasz Maher <tkmaher@umich.edu>
2024-07-12 15:30:24 -0400

    added stuff

--- social/social.js
+++ social/social.js
@@ -3,16 +3,19 @@ function sleep(ms) {
 }
 
 var tick = 0;
+var arena = document.getElementById("arena");
 var ig = document.getElementById("ig");
 var vimeo = document.getElementById("vimeo");
 var twitter = document.getElementById("twitter");
 
 async function spin() {
+    var seed0 = Math.random() * 360, seed1 = Math.random() * 360, seed2 = Math.random() * 360, seed3 = Math.random() * 360;
     while(true) {
         await sleep(10);
-        ig.style.transform = ' translate(-50%, -50%) rotate(' + ((tick*0.1) % 360) + 'deg)'
-        vimeo.style.transform = ' translate(-50%, -50%) rotate(' + ((tick*-0.2) % 360) + 'deg)'
-        twitter.style.transform = ' translate(-50%, -50%) rotate(' + ((tick*0.25) % 360) + 'deg)'
+        arena.style.transform = ' translate(-50%, -50%) rotate(' + ((tick*-0.09) + seed0) + 'deg)'
+        ig.style.transform = ' translate(-50%, -50%) rotate(' + ((tick*0.1) + seed1) + 'deg)'
+        vimeo.style.transform = ' translate(-50%, -50%) rotate(' + ((tick*-0.2) + seed2) + 'deg)'
+        twitter.style.transform = ' translate(-50%, -50%) rotate(' + ((tick*0.25) + seed3) + 'deg)'
         tick++;
     }
 }
