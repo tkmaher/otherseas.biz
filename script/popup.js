408762067422c1b40e29e6954e342e024bcd07f1
Tomasz Maher <tkmaher@umich.edu>
2024-08-10 12:45:53 -0400

    yay

--- script/popup.js
+++ script/popup.js
@@ -1,3 +1,7 @@
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; // Check https://jquery.com/ for the current version
+document.getElementsByTagName('head')[0].appendChild(script);
+
 function popup() {
 	elt = document.getElementById('thesis');
 	if (elt.style.opacity != '1') {
@@ -11,3 +15,9 @@ function popup() {
 
 document.getElementById("openInfo").addEventListener("click", popup);
 document.getElementById("closeInfo").addEventListener("click", popup);
+
+$(document).keyup(function(e) {
+	if (e.key === "Escape") { 
+		popup();
+   }
+});
