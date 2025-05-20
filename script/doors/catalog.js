23f8960249c17991f259d0fcdc0b0a5f919ef856
tkmaher <tkmaher@umich.edu>
2025-05-19 20:09:46 -0400

    bros

--- script/doors/catalog.js
+++ script/doors/catalog.js
@@ -3,6 +3,8 @@ function sleep(ms) {
 }
 
 async function loading(arr, channel) {
+  const isMobile = (/Mobi|Android|iPhone/i.test(navigator.userAgent));
+  
   var high = new Audio('../../sounds/doors/collection/high.mp3');
   high.load()
   var low = new Audio('../../sounds/doors/collection/low.mp3');
@@ -11,6 +13,10 @@ async function loading(arr, channel) {
   index = 0
   bg = document.getElementById('bg')
   txt = document.getElementById('parent')
+  
+  if (isMobile)
+    document.getElementById("cyclecontainer").style.maxWidth = "100%"
+
   str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890±!@#$%^&*()_+-=§£™¡¢∞§¶•ªº–≠œ∑´®†¥¨ˆøπ“‘åß∂ƒ©˙˚¬…æ«`~Ω≈ç√∫˜µ≤≥÷₩ÈÀÆÃŠŸÛÎÔŒŌŁŽÇÑ"
   while (true) {
     txt.innerHTML = str[index % (str.length - 1)]
@@ -21,11 +27,11 @@ async function loading(arr, channel) {
       await sleep(500)
     }
     if (Math.random() < 0.01)
-      txt.style.fontSize = "3000px"
+      txt.style.fontSize = "1200vh"
     if (Math.random() < 0.01)
-      txt.style.fontSize = "6000px"
+      txt.style.fontSize = "800vh"
     if (Math.random() < 0.01)
-      txt.style.fontSize = "5000px"
+      txt.style.fontSize = "1000vh"
     if (Math.random() < 0.01) {
       if (bg.hidden) {
         high.load()
