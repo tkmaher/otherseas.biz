b145f612e59347c32e2d27eab3a372ace8ec1f17
tkmaher <tkmaher@umich.edu>
2025-08-26 18:43:52 -0500

    go

--- script/diary/diary.js
+++ script/diary/diary.js
@@ -14,7 +14,91 @@ class entry {
 }
 
 const entries = [
-    new entry("Insect poem", "6/18/2025",
+    new entry("Dear diary,", "10-7-2024", "Me testing my blog"),
+
+    new entry("Listening to", "10-7-2024", 
+    "<a href=https://www.youtube.com/watch?v=zU1MUqD2q04>Listening to</a>"),
+
+    new entry("Song", "10-28-2024", 
+    "When the statues have all crumbled<br> \
+    And the world is full of nothing<br> \
+    And the rain is always running<br> \
+    Down a weirdly giant mirror<br><br> \
+    When the stage has nothing on it<br> \
+    And the puppets bow their faces<br> \
+    And the insects fall to pieces<br> \
+    Then my star will soon appear<br><br> \
+    When the sky was dark and silent<br> \
+    And the watchmen were all sleeping<br> \
+    Just the haruspex was looking<br> \
+    As I skulked across the floor<br><br> \
+    Past a thousand doors I hurried<br> \
+    And a hundred leagues I traveled<br> \
+    And the moon rose up behind me<br> \
+    Just to shine upon your door<br><br> \
+    And your face was made of silver<br> \
+    And I thought that you were sleeping<br> \
+    And I ran up to your bedside <br> \
+    While I left the door ajar<br><br> \
+    Then a tremor split the heavens<br> \
+    And a mirror fell from its mounting<br> \
+    And within its broken pieces<br> \
+    I could only see my star"),
+
+    new entry("Happy 2025!", "1-17-2025", 
+    "Édouard Manet. <i>The Execution of Emperor Maximilian</i>. 1868<br><br> \
+    I updated this blog to have an archive/index feature. Click \"Grid View\" at the top.", 
+    "https://d2w9rnfcy7mm78.cloudfront.net/33771627/original_f5e0d2dd6f956cc81092e57d537c68de.jpg?1737170219?bc=0"),
+    
+    new entry("radio show", "4-30-2025",
+    "Listen to my radio show with faulkner on <a href=https://reduxrad.io/shows/loisirs>reduxrad.io</a>",
+    "https://s3.us-east-005.backblazeb2.com/redux-test-bucket/hostimages/Loisirs.png"),
+
+    new entry("Green Poem", "5-21-2025",
+    `On my way home I saw something green on the ground, it was your spine<br> \
+    I was surprised to see it on the ground<br> \
+    Ants and flies weaved vestigial buta on its surface<br><br> \
+    I picked up your spine very carefully, so that it wouldn’t crumble to dust, and looked everywhere for you<br> \
+    But you weren’t anywhere<br><br> \
+    When I got home I walked a careful path around clean piles of dirt<br> \
+    I carelessly tossed your fingers into a plastic bag and put it under the stairs<br> \
+    I figured that if you weren’t anywhere, you wouldn’t need them anyway<br> \
+    So it didn’t really matter where I put them<br><br> \
+    Ten years earlier you were sleeping. I was awake<br> \
+    I extended my arms upward, and slowly lifted myself out of the bed<br> \
+    I was basically hanging by the tips of my fingernails<br> \
+    But when it was time to move my feet, I went too hastily<br> \
+    With a loud sound of shattering concrete, I tripped over your eyelashes<br> \
+    You were mad but I knew we would end up laughing about it<br><br> \
+    The green moon draws facsimile legatos on the sky<br> \
+    Seven years have passed and it has yet to complete one stanza`),
+
+    new entry("Law", "5-25-2025",
+    `Even then I could feel it<br>
+    Whispered in the flickering of the fluorescent lamp<br>
+    In the hesitation of the scalpel<br>
+    In the nervous darting of my patients’ eyes<br><br>
+    In my Eden, in my garden of first things<br>
+    Even there, I could feel the teeth of the law<br><br>
+    Shambling like a fucked-out parade<br>
+    The hooked beak of the judge<br>
+    His talons stroking the gavel<br>
+    The movement of restless wings under his darkening robe<br><br>
+    Sinking further in, rising up to efface my light, moreover:<br>
+    The jury, like so many rats<br>
+    Raping the air with their barbed words<br>
+    And the sentence —<br>
+    My face pressed up against cold glass <br>
+    Shaved heads and telephone wire-marks on our wrists<br>
+    Mountains of gravel ground down between our teeth <br>
+    And finally hoisted<br>
+    Skin stretched for leagues across a red lattice, linked together by strands of saliva<br>
+    Bodies piling up to challenge the sun<br><br>
+    Even then, the rushing noise of the deluge would build up in my ear<br>
+    Sometimes it would get so loud I couldn’t even think<br>
+    I’d force myself to ignore it, and call in the next patient`),
+
+    new entry("Insect poem", "6-18-2025",
     `In the cool shade of the Golden Pavilion, those insects who only come out under the cover of darkness held a tribunal<br>
     To amend the rights of the dead and dying, and to deliver them<br>
     Further into their strange dark world, and away from our own<br>
@@ -78,100 +162,16 @@ const entries = [
     The moon flickered in the sky, and they breathlessly spun up their ultimate verdict<br>
     I was asleep<br>
     I was deep under the ocean`),
-
-    new entry("Law", "5/25/2025",
-    `Even then I could feel it<br>
-    Whispered in the flickering of the fluorescent lamp<br>
-    In the hesitation of the scalpel<br>
-    In the nervous darting of my patients’ eyes<br><br>
-    In my Eden, in my garden of first things<br>
-    Even there, I could feel the teeth of the law<br><br>
-    Shambling like a fucked-out parade<br>
-    The hooked beak of the judge<br>
-    His talons stroking the gavel<br>
-    The movement of restless wings under his darkening robe<br><br>
-    Sinking further in, rising up to efface my light, moreover:<br>
-    The jury, like so many rats<br>
-    Raping the air with their barbed words<br>
-    And the sentence —<br>
-    My face pressed up against cold glass <br>
-    Shaved heads and telephone wire-marks on our wrists<br>
-    Mountains of gravel ground down between our teeth <br>
-    And finally hoisted<br>
-    Skin stretched for leagues across a red lattice, linked together by strands of saliva<br>
-    Bodies piling up to challenge the sun<br><br>
-    Even then, the rushing noise of the deluge would build up in my ear<br>
-    Sometimes it would get so loud I couldn’t even think<br>
-    I’d force myself to ignore it, and call in the next patient`),
-    
-    new entry("Green Poem", "5/21/2025",
-    `On my way home I saw something green on the ground, it was your spine<br> \
-    I was surprised to see it on the ground<br> \
-    Ants and flies weaved vestigial buta on its surface<br><br> \
-    I picked up your spine very carefully, so that it wouldn’t crumble to dust, and looked everywhere for you<br> \
-    But you weren’t anywhere<br><br> \
-    When I got home I walked a careful path around clean piles of dirt<br> \
-    I carelessly tossed your fingers into a plastic bag and put it under the stairs<br> \
-    I figured that if you weren’t anywhere, you wouldn’t need them anyway<br> \
-    So it didn’t really matter where I put them<br><br> \
-    Ten years earlier you were sleeping. I was awake<br> \
-    I extended my arms upward, and slowly lifted myself out of the bed<br> \
-    I was basically hanging by the tips of my fingernails<br> \
-    But when it was time to move my feet, I went too hastily<br> \
-    With a loud sound of shattering concrete, I tripped over your eyelashes<br> \
-    You were mad but I knew we would end up laughing about it<br><br> \
-    The green moon draws facsimile legatos on the sky<br> \
-    Seven years have passed and it has yet to complete one stanza`),
-
-    new entry("radio show", "4/30/2025",
-    "Listen to my radio show with faulkner on <a href=https://reduxrad.io/shows/loisirs>reduxrad.io</a>",
-    "https://s3.us-east-005.backblazeb2.com/redux-test-bucket/hostimages/Loisirs.png"),
-
-    new entry("Happy 2025!", "1/17/2025", 
-    "Édouard Manet. <i>The Execution of Emperor Maximilian</i>. 1868<br><br> \
-    I updated this blog to have an archive/index feature. Click \"Grid View\" at the top.", 
-    "https://d2w9rnfcy7mm78.cloudfront.net/33771627/original_f5e0d2dd6f956cc81092e57d537c68de.jpg?1737170219?bc=0"),
-    
-    new entry("Song", "10/28/2024", 
-    "When the statues have all crumbled<br> \
-    And the world is full of nothing<br> \
-    And the rain is always running<br> \
-    Down a weirdly giant mirror<br><br> \
-    When the stage has nothing on it<br> \
-    And the puppets bow their faces<br> \
-    And the insects fall to pieces<br> \
-    Then my star will soon appear<br><br> \
-    When the sky was dark and silent<br> \
-    And the watchmen were all sleeping<br> \
-    Just the haruspex was looking<br> \
-    As I skulked across the floor<br><br> \
-    Past a thousand doors I hurried<br> \
-    And a hundred leagues I traveled<br> \
-    And the moon rose up behind me<br> \
-    Just to shine upon your door<br><br> \
-    And your face was made of silver<br> \
-    And I thought that you were sleeping<br> \
-    And I ran up to your bedside <br> \
-    While I left the door ajar<br><br> \
-    Then a tremor split the heavens<br> \
-    And a mirror fell from its mounting<br> \
-    And within its broken pieces<br> \
-    I could only see my star"),
-
-    new entry("Listening to", "10/7/2024", 
-    "<a href=https://www.youtube.com/watch?v=zU1MUqD2q04>Listening to</a>"),
-
-    new entry("Dear diary,", "10/7/2024", "Me testing my blog")
 ]
 
-const url = "https://www.otherseas1.com/diary/"
+const url = "https://www.otherseas1.com/diary"
 const urlParams = new URLSearchParams(window.location.search);
 var page = parseInt(urlParams.get('p'));
 if (page == null) {
-    page = 0;
+    page = entries.length - 1;
     reload(page);
 } else if (!Number.isInteger(page)) {
-    page = 0;
+    page = entries.length - 1;
     reload(page);
 } else if (page >= entries.length) {
     page = entries.length - 1;
@@ -201,7 +201,7 @@ function init() {
     i = page;
     var n = document.createElement("div")
     parent.append(n)
-    n.innerHTML = "<em'>" + entries[i].date + "</em><p><strong>" 
+    n.innerHTML = "<em>" + entries[i].date + "</em><p><strong>" 
                     + entries[i].title + "</strong></p><p>" 
     if (entries[i].img) {
         n.innerHTML += "<img style='max-width:100%; width:70vh' src='" + entries[i].img + "'><br>"
@@ -243,16 +243,16 @@ function reload(newpage) {
 }
 
 function goNew() {
-    if (page >0) {
-        page--;
+    if ((page+1) < entries.length) {
+        page++;
         reload(page);
         init();
     }
 }
 
 function goOld() {
-    if ((page+1) < entries.length) {
-        page++; 
+    if ((page-1) > 0) {
+        page--; 
         reload(page);
         init();
     }
