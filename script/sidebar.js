cc75d9dfa8aa70c56b6644b91d3b53f67c1aa4ae
tkmaher <tkmaher@umich.edu>
2025-09-23 14:26:07 -0500

    test

--- script/sidebar.js
+++ script/sidebar.js
@@ -35,4 +35,32 @@ document.getElementById("sidebar").innerHTML = `
                 </div>
                 <a class="button" href="/shoppe/index.html">Shoppe</a><br><br>
             </div>
-            `;
+            ` + document.getElementById("sidebar").innerHTML;
+
+
+function collapse(target) {
+    let next = target.nextSibling;
+    for (var timeout = 0; timeout < 1000; timeout++) {
+        if (next.className != "collapseDiv") {
+            next = next.nextSibling;
+        } else {
+            break;
+        }
+    }
+    if (next.style.display == "none") {
+        next.style.display = "block";
+    } else {
+        next.style.display = "none";
+    }
+}
+
+function collapseHandler(e) {
+    let target = e.target;
+    collapse(target);
+}
+
+var collapses = document.getElementsByClassName("collapse");
+var lst = new Array(collapses.length).fill(false);
+for (let i = 0; i < collapses.length; i++) {
+    collapses[i].addEventListener("click", collapseHandler);
+}
