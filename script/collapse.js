5f1f64a777ccbbce77798e7de627bb8218c6985a
tkmaher <tkmaher@umich.edu>
2025-09-23 13:27:46 -0500

    stuff

--- script/collapse.js
+++ script/collapse.js
@@ -8,33 +8,22 @@ function collapse(target) {
             break;
         }
     }
-    if (target.innerHTML.slice(-3) == "[+]") {
-        
+    if (next.style.display == "none") {
         next.style.display = "block";
-        target.innerHTML = target.innerHTML.replace("+","-");
     } else {
         next.style.display = "none";
-        target.innerHTML = target.innerHTML.replace("-","+");
     }
 }
 
 function collapseHandler(e) {
     let target = e.target;
     collapse(target);
-    if (target.id != current) {
-        let other = document.getElementById(current);
-        if (other.innerHTML.slice(-3) == "[-]") {
-            collapse(other);
-        }
-        current = target.id;
-    }
 }
 
-//requires: collapse buttons have unique ID numbers, first ID must be "1", and be of class "collapse"
-//Every collapse button must have a corresponding div after it with classname "collapseDiv" 
 
-var current = "1";
+
 var collapses = document.getElementsByClassName("collapse");
+var lst = new Array(collapses.length).fill(false);
 for (let i = 0; i < collapses.length; i++) {
     collapses[i].addEventListener("click", collapseHandler);
 }
---
+++ script/sidebar.js
@@ -0,0 +1,38 @@
+document.getElementById("sidebar").innerHTML = `
+            <div id="9items">
+                <span class="collapse">Info</span> <br> 
+                <div class="collapseDiv" style="display:none">
+                    <a class="button" href="/about/index.html">about</a> <br> 
+                    <a class="button" href="/cv/index.html">CV</a> <br> 
+                    <a class="button" href="/halloffame/index.html">Links</a> <br> 
+                    <a class="button" href="/poetics/index.html">Poetic's</a> <br> 
+                    <a class="button" href="/putrescense/index.html">putrescense</a> <br> 
+                    <a class="button" href="/lostfound/index.html">lost && found</a> <br> 
+                    <a class="button" href="/diary/index.html">Diaries</a> <br> 
+                </div>
+                <span class="collapse">Projects</span> <br> 
+                <div class="collapseDiv" style="display:none">
+                    <a class="button" href="/siris/index.html">Siris</a> <br> 
+                    <a class="button" href="/proj/everything/index.html">everything</a> <br> 
+                    <a class="button" href="/proj/blenderDJ/index.html">BlenderDJ</a> <br> 
+                    <a class="button" href="/proj/the ruined temple/index.html">the ruined temple</a> <br> 
+                    <a class="button" href="/proj/tree-notation/index.html">Tree-notation I</a> <br> 
+                    <a class="button" href="/proj/doors/index.html">doors</a> <br> 
+                </div>
+                <span class="collapse">Art</span> <br> 
+                <div class="collapseDiv" style="display:none">
+                    <a class="button" href="/art+music/2d.html">Drawings</a> <br> 
+                    <a class="button" href="/art+music/3d.html">Renders</a> <br> 
+                    <a class="button" href="/art+music/other.html">other/real life</a> <br> 
+                    <a class="button" href="/art+music/games.html">games</a> <br> 
+                    <a class="button" href="/art+music/Music.html">Music</a> <br> 
+                </div>
+                <span class="collapse">Contacts</span> <br> 
+                <div class="collapseDiv" style="display:none">
+                    <a class="button" href="/mailto:os@otherseas1.com" target="_blank">Email</a><br> 
+                    <a class="button" href="/https://github.com/tkmaher?tab=repositories" target="_blank">GitHub</a><br> 
+                    <a class="button" href="/social/index.html">Social</a> <br>
+                </div>
+                <a class="button" href="/shoppe/index.html">Shoppe</a><br><br>
+            </div>
+            `;
