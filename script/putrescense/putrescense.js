5cd73c22d1a6b39358912584ad4e364f4c721473
tkmaher <tkmaher@umich.edu>
2025-09-26 00:16:38 -0500

    rise

--- script/putrescense/putrescense.js
+++ script/putrescense/putrescense.js
@@ -1,5 +1,4 @@
 var img = document.getElementById("src");
-var parent = document.getElementById("parent");
 var parentbook = document.getElementById("parentbook");
 
 var current = 0;
@@ -8,17 +7,6 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function spark() {
-  var spark = document.createElement("div");
-  spark.className = "spark";
-  spark.innerHTML = "☆";
-  parent.appendChild(spark);
-  await sleep(1);
-  spark.style.transform = "rotate(" + (Math.random() * 360) + "deg)";
-  spark.style.fontSize = ((Math.random() * 3) + 1) + "vh";
-  spark.style.left = (Math.random() * 100) + '%';
-  spark.style.top = (Math.random() * 100) + '%';
-}
 
 function pageturn(e) {
   if (e.offsetX > (parentbook.clientWidth / 2)) {
@@ -26,14 +14,12 @@ function pageturn(e) {
     if (current < 11) {
       img.src = src;
       current += 1;
-      spark();
     }
   } else {
     var src = "../images/putrescense/" + (current - 1) + ".png";
     if (current > 0) {
       img.src = src;
       current -= 1;
-      spark();
     }
   }
 }
